a636d1a942b8f69c4df8d48d5268bb1ff2dfe2cc
baris_000 <barisozcetin@gmail.com>
2018-05-17 14:21:37 +0300

    connected to db

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+const dbKey = require("./keys/database");
 
 const app = express();
+
+// connect to mlab
+mongoose.connect(dbKey);
+mongoose.connection.once("open", () => {
+  console.log("connected to db");
+});
 
 app.use(
   "/graphql",
